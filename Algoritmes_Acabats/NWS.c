3ef2c2bd4b076934385c458d60d0b8c63f9ff498
Armand <56553013+armandciutat@users.noreply.github.com>
2020-05-20 16:08:30 +0200

    Update NWS.c

--- Algoritmes_Acabats/NWS.c
+++ Algoritmes_Acabats/NWS.c
@@ -25,10 +25,10 @@ int main(int argc, char *argv[]) {
 void sequenceAlignmentSimple(char sequence1[], char sequence2[], int returnTraceback) {
     int gap = 7;
     int miss_match[][4] = {{0,2,3,4}, {2,0,5,1}, {3,5,0,1}, {4,3,1,0}};
-	int top_line[strlen(sequence2) + 1];
+    int top_line[strlen(sequence2) + 1];
     int current_line[strlen(sequence2) + 1];
-	int miss_match_value;
-	int current_min;
+    int miss_match_value;
+    int current_min;
 
     top_line[0] = 0;
 
@@ -55,21 +55,21 @@ void sequenceAlignmentSimple(char sequence1[], char sequence2[], int returnTrace
     printf("%i\n",top_line[strlen(sequence2)]);
 }
 
-int min(int a,int b){
-    if(a < b){
+int min(int a,int b) {
+    if(a < b) {
         return a;
     }
     return b;
 }
 
-int orderFunction(char character){
-	if (character == 'A'){
+int orderFunction(char character) {
+	if (character == 'A') {
         return 0;    
-    }else if (character == 'G'){
+    } else if (character == 'G') {
         return 1;
-    }else if (character == 'C'){
+    } else if (character == 'C') {
         return 2;
-    }else{
+    } else {
         return 3;
     }
 }
